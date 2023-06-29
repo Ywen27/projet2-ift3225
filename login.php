48e4c90bdc01a941242a38d834898e25e988bceb
Wen Yin <andywenyin@gmail.com>
2023-06-29 15:42:31 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1));
+
+session_start();
+
+include('connectionDB.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM Users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    // Vérifiez si le mot de passe est correct
+    if (password_verify($password, $user['password'])) {
+        // Le mot de passe est correct, connectez l'utilisateur
+        $_SESSION['user_id'] = $user['id'];
+        header('Location: dashboard.php');
+        exit;
+    } else {
+        // Le mot de passe est incorrect, affichez une erreur
+        $error = 'Le nom d\'utilisateur ou le mot de passe est incorrect';
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+
+    <?php if (isset($error)): ?>
+        <p><?php echo $error; ?></p>
+    <?php endif; ?>
+
+    <form method="post" action="login.php">
+        <label for="username">Nom d'utilisateur:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="password">Mot de passe:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+</html>
