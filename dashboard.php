bd4c2bb561905e7f2c45ef78bf4a4dc10932994c
Wen Yin <andywenyin@gmail.com>
2023-06-30 03:40:59 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,16 @@
 <?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1));
 
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+include('connectionDB.php');
+
+
+
+
+
 mysqli_close($conn) or die("Probleme lors de la fermeture de la connection ". msqli_error());
 
 echo "Test\n"
