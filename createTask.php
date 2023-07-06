7440b4c96650bcb7c34919af9d63504d6e30ffc1
Wen Yin <andywenyin@gmail.com>
2023-07-06 04:06:14 -0400

    Update createTask.php

--- createTask.php
+++ createTask.php
@@ -4,14 +4,16 @@ include('connectionDB.php');
 
 $response = array("success" => false);
 
-if (isset($_SESSION['user_id'], $_POST['task-title'], $_POST['task-start-date'], $_POST['task-category'], $_POST['task-description'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_SESSION['user_id'], $_POST['task-title'], $_POST['task-start-date'], $_POST['task-category'], $_POST['task-description'])) {
 
-    $stmt = $conn->prepare("INSERT INTO taches (nom_tache, user_id, date_debut, categorie_id, description) VALUES (?, ?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO taches (nom_tache, user_id, date_debut, categorie_id, description) VALUES (?, ?, ?, ?, ?)");
 
-    $stmt->bind_param("sisss", $_POST['task-title'], $_SESSION['user_id'], $_POST['task-start-date'], $_POST['task-category'], $_POST['task-description']);
+        $stmt->bind_param("sisss", $_POST['task-title'], $_SESSION['user_id'], $_POST['task-start-date'], $_POST['task-category'], $_POST['task-description']);
 
-    if ($stmt->execute()) {
-        $response["success"] = true;
+        if ($stmt->execute()) {
+            $response["success"] = true;
+        }
     }
 }
 
