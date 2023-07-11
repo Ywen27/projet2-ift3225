720900c943029a5467e1f45f12bda1d6bf9d558e
Wen Yin <andywenyin@gmail.com>
2023-07-11 00:59:57 -0400

    update

--- pwdDB.php
+++
@@ -1,3 +0,0 @@
-<?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1));
-$db_password = "2002Wen02Yin27@";
-?>
---
+++ src/connectionDB.php
@@ -0,0 +1,14 @@
+<?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1));
+
+include("pwdDB.php");
+
+$db_user = "yinwen";
+$db_host = "www-ens.iro.umontreal.ca";
+$db_name = "yinwen_projet2_ift3225";
+
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
